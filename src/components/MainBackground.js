cb063c59fe37ed704af36d08da921887822cfe87
Serena <dserena01@yahoo.com>
2022-11-21 01:28:29 -0600

    Replace bg color with video bg

--- src/components/MainBackground.js
+++ src/components/MainBackground.js
@@ -1,6 +1,7 @@
 export default function MainBackground() {
     return(
         <div className="main-bg absolute bg-zinc-800 m-auto left-0 right-0 w-10/12 min-h-screen">
+            <video className="object-cover w-full h-screen overflow-hidden" src="https://assets.mixkit.co/videos/preview/mixkit-triangular-tunnel-with-blue-lights-in-its-walls-34317-large.mp4" autoPlay muted playsInline loop/>
         </div>
     )
 };
