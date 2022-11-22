b42088f49050aeb81ce1a93b798f0c85d1defde9
Serenuy <dserena01@yahoo.com>
2022-11-22 16:46:31 -0600

    Progress work page, modal wip

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,3 +1,5 @@
+import FeatherIcon from 'feather-icons-react';
+
 export default function Work() {
 	return(
 		// create popup for each project
@@ -8,21 +10,29 @@ export default function Work() {
 			<div className="text-white flex flex-wrap h-screen justify-center items-center gap-5 lg:gap-20">
 				<div>
 					<div className="relative mb-5 bg-neutral-900 p-20 bg-cover bg-no-repeat bg-center border-4 overflow-hidden rounded-md shadow-lg shadow-black lg:p-40 lg:border-8" style={{backgroundImage: "url('https://res.cloudinary.com/dyqefy6t8/image/upload/v1665823921/ubsizpsqpqkyymxa23r6.jpg')"}}>
-						<div className="absolute text-xl font-bold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 hover:bg-red-800 drop-shadow-lg hover:bg-opacity-40 hover:text-red-500 ease-in duration-300 p-40 lg:text-4xl">EXPERTECE</div>
+						<div className="absolute text-xl font-bold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 hover:bg-red-800 drop-shadow-lg hover:bg-opacity-40 hover:text-red-500 ease-in duration-300 p-40 lg:text-4xl">EXPERTECE
+							<span className="text-lg cursor-pointer block hover:underline hover:text-white">Learn More <FeatherIcon className="inline text-center" icon="arrow-right"/></span>
+						</div>
 					</div>
 					<div className="relative bg-neutral-900 p-20 bg-cover bg-no-repeat bg-center overflow-hidden border-4 rounded-md shadow-lg shadow-black lg:p-40 lg:border-8" style={{backgroundImage: "url('images/aiBrain.jpg')"}}>
-						<div className="absolute text-xl font-bold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 hover:bg-black drop-shadow-lg hover:bg-opacity-40 hover:text-sky-300 ease-in duration-300 p-40 lg:text-4xl">FACE ANALYZER</div>
+						<div className="absolute text-xl font-bold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 hover:bg-black drop-shadow-lg hover:bg-opacity-40 hover:text-sky-300 ease-in duration-300 p-40 lg:text-4xl">FACE ANALYZER
+							<span className="text-lg cursor-pointer block hover:underline hover:text-white">Learn More <FeatherIcon className="inline text-center" icon="arrow-right"/></span>
+						</div>
 					</div>
 				</div>
 
 				<div>
 					<div className="relative mb-5 bg-neutral-900 p-20 bg-cover bg-scroll bg-no-repeat overflow-hidden bg-center border-4 rounded-md shadow-lg shadow-black lg:p-40 lg:border-8" style={{backgroundImage: "url('https://rtf-prototype.onrender.com/logo.png')"}}>
-						<div className="absolute text-xl font-bold drop-shadow-lg top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 hover:text-orange-500 hover:bg-black hover:bg-opacity-40 ease-in duration-300 p-40 lg:text-4xl">REDEMPTION TEMPLE FITNESS</div>
+						<div className="absolute text-xl font-bold drop-shadow-lg top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 hover:text-orange-500 hover:bg-black hover:bg-opacity-40 ease-in duration-300 p-40 lg:text-4xl">REDEMPTION TEMPLE FITNESS
+							<span className="text-lg cursor-pointer block hover:underline hover:text-white">Learn More <FeatherIcon className="inline text-center" icon="arrow-right"/></span>
+						</div>
 					</div>
 					<div className="relative bg-neutral-900 p-20 bg-cover bg-no-repeat bg-center overflow-hidden border-4 rounded-md shadow-lg shadow-black lg:p-40 lg:border-8" style={{backgroundImage: "url('images/beachcover.png')"}}>
 						<video className="absolute object-cover h-96 bottom-0 left-0 hover:bg-black hover:bg-opacity-40 ease-in duration-300" src="https://assets.mixkit.co/videos/preview/mixkit-top-aerial-shot-of-seashore-with-rocks-1090-large.mp4" onMouseOver={(e) => e.target.play()} onMouseOut={(e) =>e.target.pause()} ></video>
 						
-						<div className="absolute drop-shadow-lg text-xl font-bold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 lg:text-4xl">TRAVEL DREAMS</div>
+						<div className="absolute drop-shadow-lg text-xl font-bold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 lg:text-4xl">TRAVEL DREAMS
+							<span className="text-lg cursor-pointer block hover:underline">Learn More <FeatherIcon className="inline text-center" icon="arrow-right"/></span>
+						</div>
 					</div>
 				</div>
 			</div>
