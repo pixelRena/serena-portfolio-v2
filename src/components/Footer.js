8af3e6d5b745e4d15f5afbdece85ed5ae92b1fa0
Serena <dserena01@yahoo.com>
2022-12-08 22:25:51 -0600

    Test layout heroku

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { TbBrandTailwind } from 'react-icons/tb';
 
 export default function Footer() {
 	return(
-		<footer className="text-center text-white p-4">
+		<footer className="text-center text-white p-4 mt-16">
 			<span className="flex justify-center mb-2"><FeatherIcon icon="heart"/></span>
 			<span>Made with <a href="https://github.com/pixelRena/serena-portfolio-v2" className="text-sky-500 hover:underline">Passion</a></span>
 			<span className="block">Buit with <FaReact title="react" className="inline text-sky-500" size="20"/> <FaFigma title="figma" className="inline text-pink-500" size="20"/> <TbBrandTailwind title="tailwind" className="inline text-blue-700" size="20"/> </span>
