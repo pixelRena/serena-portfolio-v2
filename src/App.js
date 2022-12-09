e76bc4fd870abeca533204520f3cd72f1f1d30dd
Serena <dserena01@yahoo.com>
2022-12-08 22:20:13 -0600

    Test layout heroku

--- src/App.js
+++ src/App.js
@@ -18,14 +18,10 @@ function App() {
         <TopNav/>
         <Routes>
           <Route path="/" element={<Home/>}/>
-        </Routes>
-        <div className="mt-16">
-        <Routes>
           <Route path="/about" element={<About/>}/>
           <Route path="/contact" element={<Contact/>}/>
           <Route path="/creations" element={<Work/>}/>
         </Routes>
-        </div>
         <Footer/>
     </div>
     </BrowserRouter>
