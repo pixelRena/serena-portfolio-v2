c519266c203c78fa8bff57229015f79e6f834461
Serena <dserena01@yahoo.com>
2022-12-10 03:35:05 -0600

    Update about screen

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,7 +10,7 @@ export default function About() {
 			<div className="col-span-2 mt-32 w-48 h-48 rounded-full border-8 border-neutral-800 shadow-2xl shadow-black justify-self-center lg:w-96 lg:h-96 lg:col-span-1">
 				<img className="overflow-hidden top-0 left-0 rounded-full" alt="avatar" src="https://avatars.githubusercontent.com/u/98000871?v=4"/>
 			</div>
-			<div className="col-span-2">
+			<div className="mt-32 col-span-2">
 				<h1 className="text-2xl font-semibold lg:text-6xl"><span className="text-sky-500">Serena</span> Dean</h1>
 				<p className="text-sm p-5 text-center tracking-tight lg:text-xl lg:mt-7 lg:tracking-wide">Hola! I'm a Freelancing Web Developer wanting to transfer over to Front-End Developement. From a 3D artist to creating codes and bringing them to life in web development, I enjoy every aspect of designing websites including user-friendliness, attractiveness, and simplicity. Want to connect, get a quote or have coffee together and chat? Feel free to contact me, my inbox is open to all and I'll get back with you as soon as possible.
 				</p>
@@ -19,57 +19,6 @@ export default function About() {
 					<Link to="/creations"><button className="mr-2 border-2 border-white rounded-lg font-semibold p-2 hover:bg-white hover:bg-opacity-30 text-sm mt-2 lg:mt-0 lg:inline lg:p-3 lg:text-lg">Discover my work</button></Link>
 				</div>
 			</div>
-			{/* <div className="col-span-3">
-				<h1 className="text-2xl font-semibold lg:text-6xl text-sky-500">Kn<span className='text-white'>o</span>wledge</h1>
-			</div>
-
-			<div className="col-span-3 lg:col-start-2 lg:col-end-2">
-				<div className="flex mb-1">
-					<span className="font-medium">HTML</span>
-				</div>
-				<div className="w-full bg-gray-200 rounded-full h-2.5">
-					<div className="bg-orange-600 h-2.5 rounded-full w-[95%]"></div>
-				</div>
-				<div className="mt-5 flex mb-1">
-					<span className="font-medium">CSS</span>
-				</div>
-				<div className="w-full bg-gray-200 rounded-full h-2.5">
-					<div className="bg-blue-600 h-2.5 rounded-full w-[85%]"></div>
-				</div>
-				<div className="mt-5 flex mb-1">
-					<span className="font-medium">Javascript</span>
-				</div>
-				<div className="w-full bg-gray-200 rounded-full h-2.5">
-					<div className="bg-yellow-600 h-2.5 rounded-full w-[82%]"></div>
-				</div>
-				<div className="mt-5 flex mb-1">
-					<span className="font-medium">React</span>
-				</div>
-				<div className="w-full bg-gray-200 rounded-full h-2.5">
-					<div className="bg-cyan-600 h-2.5 rounded-full w-[75%]"></div>
-				</div>
-				<div className="mt-5 flex mb-1">
-					<span className="font-medium">Figma</span>
-				</div>
-				<div className="w-full bg-gray-200 rounded-full h-2.5">
-					<div className="bg-pink-600 h-2.5 rounded-full w-[45%]"></div>
-				</div>
-
-				<h1 className="mt-5 text-2xl font-semibold ">Databases</h1>
-				<div className="flex mb-1">
-					<span className="font-medium">MongoDB</span>
-				</div>
-				<div className="w-full bg-gray-200 rounded-full h-2.5">
-					<div className="bg-green-600 h-2.5 rounded-full w-[65%]"></div>
-				</div>
-				<div className="mt-5 flex mb-1">
-					<span className="font-medium">PostgreSQL</span>
-				</div>
-				<div className="w-full bg-gray-200 rounded-full h-2.5">
-					<div className="bg-blue-800 h-2.5 rounded-full w-[95%]"></div>
-				</div>
-
-			</div> */}
 		</div>
 		</div>
 		</>
