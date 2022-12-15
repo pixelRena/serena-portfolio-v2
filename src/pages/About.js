7f4d0cd226ed0bd4fd2ab23fb0bc7fed6bef13e3
Serena Dean <dserena01@yahoo.com>
2022-12-15 15:48:49 -0600

    Update margins

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,7 +10,7 @@ export default function About() {
 			<div className="col-span-2 mt-32 w-48 h-48 rounded-full border-8 border-neutral-800 shadow-2xl shadow-black justify-self-center lg:mt-0 lg:w-96 lg:h-96 lg:col-span-1">
 				<img className="overflow-hidden top-0 left-0 rounded-full" alt="avatar" src="https://avatars.githubusercontent.com/u/98000871?v=4"/>
 			</div>
-			<div className="mt-32 col-span-2 lg:mt-0">
+			<div className="mt-5 col-span-2 lg:mt-0">
 				<h1 className="text-2xl font-semibold lg:text-6xl"><span className="text-sky-500">Serena</span> Dean</h1>
 				<p className="text-sm p-5 text-center tracking-tight lg:text-xl lg:mt-7 lg:tracking-wide">Hola, Hello! I'm a Freelancing Web Developer wanting to transfer over to Front-End Developement. From a 3D artist to creating codes and bringing them to life in web development, I enjoy every aspect of designing websites including user-friendliness, attractiveness, and simplicity. Want to connect, get a quote or have coffee together and chat? Feel free to contact me, my inbox is open to all and I'll get back with you as soon as possible.
 				</p>
